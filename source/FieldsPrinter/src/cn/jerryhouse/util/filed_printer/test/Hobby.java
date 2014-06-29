c4439f512d305bd11e9d582796debceae9099c73
test <test@idea-PC-jerry>
2014-06-29 22:11:53 +0800

    change package name

--- source/FieldsPrinter/src/cn/jerryhouse/util/filed_printer/test/Hobby.java
+++
@@ -1,9 +0,0 @@
-package cn.jerryhouse.util.filed_printer.test;
-
-public class Hobby {
-	String name;
-	public Hobby(String name)
-	{
-		this.name = name;
-	}
-}
--- source/FieldsPrinter/src/cn/jerryhouse/util/filed_printer/test/Teacher.java
+++
@@ -1,5 +0,0 @@
-package cn.jerryhouse.util.filed_printer.test;
-
-public class Teacher {
-	String name = "jobs";
-}
